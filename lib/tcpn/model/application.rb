fb5eb5e337e66d0670544b2904951d45509abb0d
Wojciech Rzasa <me@wojciechrzasa.pl>
2014-10-27 20:29:18 +0100

    Added event:cpu_load to application model

--- lib/tcpn/model/application.rb
+++ lib/tcpn/model/application.rb
@@ -1,8 +1,8 @@
 page 'application' do
   process = place 'process'
-#  cpu = place 'CPU'
+  cpu = place 'CPU'
 
-  transition 'delay_for' do
+  transition 'event::delay_for' do
     input process, :process
     output process do |binding, clock|
       process = binding[:process][:val]
@@ -13,6 +13,35 @@ page 'application' do
 
     guard do |binding, clock|
       binding[:process][:val].has_event? :delay_for
+    end
+  end
+
+  transition 'event::cpu' do
+    input process, :process
+    input cpu, :cpu
+
+    def processing_delay(binding)
+      process = binding[:process][:val]
+      cpu = binding[:cpu][:val]
+      event = process.serve_system_event :cpu
+      event[:args][:block].call cpu
+    end
+
+    output process do |binding, clock|
+      { val: process, ts: clock + processing_delay(binding) }
+    end
+
+    output cpu do |binding, clock|
+      { val: cpu, ts: clock + processing_delay(binding) }
+    end
+
+    guard do |binding, clock|
+      if binding[:process][:val].has_event?(:cpu) &&
+         (binding[:process][:val].node == binding[:cpu][:val].node)
+        true
+      else
+        false
+      end
     end
   end
 end
--- sim.rb
+++ sim.rb
@@ -5,20 +5,52 @@ class ProcessToken < RBSim::HLModel::Process
   include TCPN::TokenMethods
 end
 
-model = TCPN.read 'lib/tcpn/model/application.rb'
-
-processes = (1..2).map do
-  process = ProcessToken.new(:node01)
-  10.times { process.register_event(:delay_for, time: 10) }
-  process.register_event(:delay_for, time: 100)
-  model.add_marking_for 'process', process
+class CPU
+  attr_accessor :node, :performance
+  def initialize(node, performance)
+    @node, @performance = node, performance
+  end
 end
 
+class CPUToken < CPU
+  include TCPN::TokenMethods
+end
+
+class Handler
+  def call(*)
+    2345
+  end
+end
+
+def doproc(&block)
+  return block
+end
+
+model = TCPN.read 'lib/tcpn/model/application.rb'
+
+process = ProcessToken.new(:node01)
+process.with_event :data do
+  puts "*"*80
+  puts "* GOT EVENT DATA"
+  puts "*"*80
+end
+#handler = Handler.new
+handler = Proc.new { |cpu| 1000/cpu.performance } # java error if handler is set to Proc or lambda...
+#handler = Proc.new { |cpu| 1000 } # java error if handler is set to Proc or lambda...
+#handler = doproc do |cpu|
+#  1000/cpu.performance
+#end
+process.register_event(:cpu, block: handler )
+#process.register_event(:delay_for, time: 100)
+model.add_marking_for 'process', process
+
+cpu = CPUToken.new(:node01, 10)
+model.add_marking_for 'CPU', cpu
 
 sim = TCPN.sim model
 
 sim.cb_for :transition, :after do |t, e|
-  puts "#{e.transition} #{e.binding.map { |k, v| "#{k}: #{v}"}.join ',' }"
+  puts "==> FIRING: #{e.transition} #{e.binding.map { |k, v| "#{k}: #{v}"}.join ',' }"
   #puts model.marking
 end
 
