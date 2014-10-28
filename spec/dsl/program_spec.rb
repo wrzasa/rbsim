a26aa2700f8cbfc62c5c325ea300638553f2b795
Wojciech Rzasa <me@wojciechrzasa.pl>
2014-10-28 16:51:04 +0100

    DSL: spec for program statement

---
+++ spec/dsl/program_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper.rb'
+
+describe 'RBSim#dsl' do
+
+  context "with two programs" do
+
+    let(:model) do
+      RBSim.dsl do
+
+        program :waiter do |time|
+          delay_for time: time
+        end
+
+        program :worker do |volume|
+          cpu do |cpu|
+            volume * volume / cpu.performance
+          end
+        end
+
+      end
+    end
+
+    it "has two programs" do
+      expect(model.programs.size).to eq(2)
+    end
+
+    it "has program called :waiter" do
+      expect(model.programs[:waiter]).not_to be_nil
+    end
+
+    it "has program called :volume" do
+      expect(model.programs[:volume]).not_to be_nil
+    end
+
+  end
+end
