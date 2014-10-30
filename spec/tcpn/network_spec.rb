8a29288731f8b7a45ea56950d2c9ed8f57b3119b
Wojciech Rzasa <me@wojciechrzasa.pl>
2014-10-30 20:44:13 +0100

    pending specs for TCPN model of network data transmission

---
+++ spec/tcpn/network_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe "TCPN model" do
+
+  describe "page 'network'" do
+    it "puts data token with correct timestamp in 'data to receive' place"
+    it "fires net transition correct number of times"
+    it "updates timestamp of correct net tokens"
+  end
+
+end
+
