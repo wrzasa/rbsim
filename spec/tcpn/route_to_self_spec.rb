54ab5f44b597c113056ed13de01a5022d5015898
Wojciech Rzasa <me@wojciechrzasa.pl>
2014-11-03 14:03:25 +0100

    Added spec to route to self TCPN model

---
+++ spec/tcpn/route_to_self_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+
+describe "TCPN model" do
+
+  describe "page 'add route'" do
+
+    context "route from node to self" do
+      let :data_token do
+        data = RBSim::Tokens::DataToken.new(:node01, :sender, to: :worker1, size: 345, type: :req, content: :anything)
+        data.dst_node = :node01
+        data
+      end
+
+
+      let :incorrect_route1 do
+        RBSim::HLModel::Route.new :node05, :node02, [ :net04, :net03 ]
+      end
+
+      let :incorrect_route2 do
+        RBSim::HLModel::Route.new :node01, :node05, [ :net02, :net05 ]
+      end
+
+      let :routes_token do
+        routes = RBSim::Tokens::RoutesToken.new
+        routes << incorrect_route1
+        routes << incorrect_route2
+        routes
+      end
+
+      let :tcpn do
+        tcpn = TCPN.read 'tcpn/model/add_route.rb'
+
+        tcpn.add_marking_for 'data for network', data_token
+        tcpn.add_marking_for 'routes', routes_token
+        tcpn
+      end
+
+      it "adds correct route to data" do
+        TCPN.sim(tcpn).run
+
+        expect(tcpn.marking_for('data with route')).to eq(nil)
+        expect(tcpn.marking_for('data to receive')).to eq(data_token)
+      end
+
+    end
+
+  end
+end
+
+
+
