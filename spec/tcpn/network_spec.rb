6294b986a5cd612f0c84da05a9380d761f9a1c56
Wojciech Rzasa <me@wojciechrzasa.pl>
2015-12-18 23:46:21 +0100

    Added some tests to TCPN model of dropping net packets

--- spec/tcpn/network_spec.rb
+++ spec/tcpn/network_spec.rb
@@ -63,6 +63,50 @@ describe "TCPN model" do
         time = net[:ts]
       end
     end
+
+
+    describe "dropping packages" do
+
+      let :route do
+        RBSim::HLModel::Route.new :node01, :node02, [ :net01 ]
+      end
+
+      let :data_token do
+        data = RBSim::Tokens::DataToken.new(:node01, :sender, to: :worker1, size: 4000, type: :req, content: :anything)
+        data.dst_node = :node02
+        data.route = route
+        data
+      end
+
+      let :tcpn do
+        tcpn = FastTCPN.read 'tcpn/model/network.rb'
+
+        tcpn.add_marking_for 'data to receive', RBSim::Tokens::DataQueueToken.new(:worker1)
+        tcpn.add_marking_for 'data with route', data_token
+        tcpn.add_marking_for 'net', RBSim::Tokens::NetToken.new(:net01, 50, 0, drop)
+        tcpn
+      end
+
+      describe "when #drop? returns false" do
+        let(:drop) { ->{ false } }
+
+        it "does not drop package" do
+          tcpn.sim
+          expect(tcpn.marking_for('data to receive').first[:val].get).to eq data_token
+        end
+      end
+
+      describe "when #drop? returns true" do
+        let(:drop) { ->{ true } }
+
+        it "drops package" do
+          tcpn.sim
+          expect(tcpn.marking_for('data to receive').first[:val]).to be_empty
+        end
+      end
+
+    end
+
   end
 
 end
