3cb5c9de333a7d5c0f9d7b6fdd9275165acc1969
Wojciech Rzasa <me@wojciechrzasa.pl>
2016-11-08 20:59:29 +0100

    Some old uncommitted chang... :(

--- lib/rbsim/dsl.rb
+++ lib/rbsim/dsl.rb
@@ -22,7 +22,8 @@ module RBSim
   # Read model from a +file+.
   # +params+ will be passes as to the model as 'params' variable
   def self.read(file, params = {})
-    block = instance_eval "proc { |params| #{File.read file} }", file
+    p = "proc { |params| #{File.read file} }"
+    block = instance_eval p, file
     model params, &block
   end
 
